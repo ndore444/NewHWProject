b61839d0b15d4a80674cdb395c6d3052bdad4ad8
Nate D <ndore444@yahoo.com>
2018-02-06 11:44:28 -0500

    Ver. 1.0

---
+++ NewHWProject/New File.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+//function declarations
+void createProject(ifstream& in_stream, ofstream& out_stream, vector<string> _fileList);
+
+void transfer(ifstream& in_stream, ofstream& out_stream);
+
+int main( )
+{
+	ifstream fin;
+	ofstream fout;
+    vector<string> fileList = {"newProject.cpp", "makefile", "Plan.txt", "Readme.txt"};
+
+	createProject(fin, fout, fileList);
+}
+
+//function definitions
+void createProject(ifstream & in_stream, ofstream & out_stream, vector<string> _fileList)
+{
+	string temp;
+	for (int i = 0; i < _fileList.size(); i++)
+	{
+		//transferring the cpp template
+		in_stream.open("C:\\Users\\Nate\\Documents\\Visual Studio 2015\\Projects\\misc\\NewHWProject\\NewHWProject\\templates\\" + _fileList[i]);
+		if (in_stream.fail())
+		{
+			cout << "Error connecting to the in stream of " << _fileList[i] << endl;
+			system("pause");
+			exit(1);
+		}
+
+		out_stream.open("C:\\Users\\Nate\\Documents\\Visual Studio 2015\\Projects\\School\\" + _fileList[i]);
+		if (out_stream.fail())
+		{
+			cout << "Error connecting the out stream of " << _fileList[i] << endl;
+			system("pause");
+			exit(1);
+		}
+
+		transfer(in_stream, out_stream);
+
+		in_stream.close();
+		out_stream.close();
+	}
+}
+
+void transfer(ifstream& in_stream, ofstream& out_stream)
+{
+	char next;
+	in_stream.get(next);
+	while(! in_stream.eof())			//eof becuase its text based data
+	{
+		out_stream.put(next);
+		in_stream.get(next);
+	}
+}
+
+
+
+
+
---
+++ NewHWProject/templates/newProject.cpp
@@ -0,0 +1,23 @@
+/* 
+ *Name: Nathaniel Dore
+ *Class: CSCI <<CHANGE ME>>
+ *Description: [fill in description]
+ *Lab Partner: N/A
+ *Status: development complete: final release build 
+ */
+
+#include <iostream>
+using namespace std;
+
+int main( )
+{
+	/*
+	Remember to
+	rename the files! :)
+	change the date in main.cpp, and the readme
+	change the class # in main.cpp and the readme
+	change the make file objects and exe name
+	*/
+    system("Pause");
+    return 0;
+}
